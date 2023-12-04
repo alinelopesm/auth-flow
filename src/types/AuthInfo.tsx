43cbe9df3af7851815a813e48894abe0fe06ddca
alinelopesm <alinealopesm@gmail.com>
2023-12-04 09:00:52 -0300

    feat(types): Init type auth

---
+++ src/types/AuthInfo.tsx
@@ -0,0 +1,15 @@
+// authInfoInterface.tsx
+
+interface AuthInfo {
+    id: number;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    image: string;
+    token: string;
+  }
+  
+  export default AuthInfo;
+  
